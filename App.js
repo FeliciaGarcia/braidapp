0b939d3cdcac5c8d325ce1a164b4b58c5cd7dde2
Felicia Garcia <>
2018-09-12 23:07:51 -0400

    button added to card

--- App.js
+++ App.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import { View, Image, Text } from 'react-native';
 import { Card, ListItem, Button, Icon } from 'react-native-elements';
 
-export default class LotsOfStyles extends Component {
+export default class Braiders extends Component {
     render() {
         return (
           <View>
               <Card>
                   <Image source={{uri: 'https://i.imgur.com/XVrWugu.jpg'}} style={{width: 300, height: 250, }} />
-                  <Text>styles by Lisa</Text>
+                  <Text>Styles by Lisa</Text>
                   <Text>Atlanta, GA</Text>
+                  <Button
+                    backgroundColor='#ffcc00'
+                    title='book this braider'
+
+                  />
               </Card>
           </View>
         );
