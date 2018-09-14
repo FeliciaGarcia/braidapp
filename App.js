ffe0b6f7a65a1ab1ce6a949bfd5ce6c869107e0e
Felicia Garcia <>
2018-09-14 14:44:49 -0400

    initialized firebase

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 import { View, Image, Text } from 'react-native';
 import { Card, ListItem, Button, Icon } from 'react-native-elements';
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBsUTAkpJkVM2_xcK21W3cex-eaY5vesM0",
+    authDomain: "braids-app.firebaseapp.com",
+    databaseURL: "https://braids-app.firebaseio.com",
+    projectId: "braids-app",
+    storageBucket: "braids-app.appspot.com",
+    messagingSenderId: "451322916581"
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+
+function writeUserData() {
+    firebase.database().ref().set({
+        username: "Felicia",
+        email: "felicia@gmail.com",
+        profile_picture : "https://i.imgur.com/XVrWugu.jpg"
+    });
+}
 
 export default class Braiders extends Component {
+
+
     render() {
+        writeUserData();
         return (
           <View>
               <Card>
-                  <Image source={{uri: 'https://i.imgur.com/XVrWugu.jpg'}} style={{width: 300, height: 250, }} />
+                  <Image source={{uri: 'https://i.imgur.com/XVrWugu.jpg'}} style={{width: 300, height: 250,}}/>
                   <Text>Styles by Lisa</Text>
                   <Text>Atlanta, GA</Text>
                   <Button
@@ -20,6 +46,3 @@ export default class Braiders extends Component {
         );
     }
 }
-
-
-
